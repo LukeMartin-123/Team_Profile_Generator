95385974fdb70a6e316f863109ef58c12f57363f
Luke Martin <ljmart02@gmail.com>
2020-12-23 13:42:46 -0700

    Working Application

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let teamMembers = []
 
 const managerPrompt = () =>
     inquirer.prompt([
-       
+
         {
             type: 'input',
             name: 'name',
@@ -52,117 +52,121 @@ const managerPrompt = () =>
         },
 
     ])
-    .then((managerAnswer) => {
-        if (managerAnswer.role === 'Engineer'){
-            engineerPrompt()
-        }
-        else if (managerAnswer.role === 'Intern'){
-            internPrompt()
-        }
-       const manager = new Manager(managerAnswer.name, managerAnswer.Id, managerAnswer.email, managerAnswer.office);
-       teamMembers.push(manager)
-        fs.writeFile(outputPath, teamHTML, (err) => {
-            if (err) throw err;
-          });
-    })
+        .then((managerAnswer) => {
+            const manager = new Manager(managerAnswer.name, managerAnswer.Id, managerAnswer.email, managerAnswer.office);
+            teamMembers.push(manager)
+            if (managerAnswer.role === 'Engineer') {
+                engineerPrompt()
+            }
+            else if (managerAnswer.role === 'Intern') {
+                internPrompt()
+            }
+
+            else (fs.writeFile(outputPath, render(teamMembers), (err) => {
+                if (err) throw err;
+            }))
+
+        })
 
 const engineerPrompt = () =>
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the name of the engineer?',
-            },
-    
-            {
-                type: 'input',
-                name: 'Id',
-                message: 'What is their ID number?',
-            },
-    
-            {
-                type: 'input',
-                name: 'email',
-                message: 'What is their email address?',
-            },
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the engineer?',
+        },
 
-            {
-                type: 'input',
-                name: 'Github',
-                message: 'What is your Github Username?',
-             },
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is their ID number?',
+        },
 
-             {
-                type: 'list',
-                name: 'role',
-                message: 'What type of employee would you like to add next?',
-                choices: ['Engineer', 'Intern', 'Do not add another employee'],
-            },
-        ])
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is their email address?',
+        },
+
+        {
+            type: 'input',
+            name: 'Github',
+            message: 'What is your Github Username?',
+        },
+
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What type of employee would you like to add next?',
+            choices: ['Engineer', 'Intern', 'Do not add another employee'],
+        },
+    ])
         .then((engineerAnswer) => {
-            if (engineerAnswer.role === 'Engineer'){
+            const engineer = new Engineer(engineerAnswer.name, engineerAnswer.Id, engineerAnswer.email, engineerAnswer.Github);
+            teamMembers.push(engineer)
+            if (engineerAnswer.role === 'Engineer') {
                 engineerPrompt()
             }
-            else if (engineerAnswer.role === 'Intern'){
+            else if (engineerAnswer.role === 'Intern') {
                 internPrompt()
             }
-            const engineer = new Engineer (engineerAnswer.name, engineerAnswer.Id, engineerAnswer.email, engineerAnswer.Github);
-            teamMembers.push(engineer)
-            fs.writeFile(outputPath, teamHTML, (err) => {
+            else (fs.writeFile(outputPath, render(teamMembers), (err) => {
                 if (err) throw err;
-              });
+            }))
+
         })
-        
+
 
 const internPrompt = () =>
-  inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the name of the intern?',
-            },
-    
-            {
-                type: 'input',
-                name: 'Id',
-                message: 'What is their ID number?',
-            },
-    
-            {
-                type: 'input',
-                name: 'email',
-                message: 'What is their email address?',
-            },
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the intern?',
+        },
 
-            {
-                type: 'input',
-                name: 'school',
-                message: 'What school do they attend?',
-            },
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is their ID number?',
+        },
 
-            {
-                type: 'list',
-                name: 'role',
-                message: 'What type of employee would you like to add next?',
-                choices: ['Engineer', 'Intern', 'Do not add another employee'],
-            },
-        ])
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is their email address?',
+        },
+
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school do they attend?',
+        },
+
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What type of employee would you like to add next?',
+            choices: ['Engineer', 'Intern', 'Do not add another employee'],
+        },
+    ])
         .then((internAnswer) => {
-            if (internAnswer.role === 'Engineer'){
-                engineerPrompt()
-            }
-            else if (internAnswer.role === 'Intern'){
-                internPrompt()
-            }
             const intern = new Intern(internAnswer.name, internAnswer.Id, internAnswer.email, internAnswer.school);
             teamMembers.push(intern)
-            fs.writeFile(outputPath, teamHTML, (err) => {
+            if (internAnswer.role === 'Engineer') {
+                engineerPrompt()
+            }
+            else if (internAnswer.role === 'Intern') {
+                internPrompt()
+            }
+            else (fs.writeFile(outputPath, render(teamMembers), (err) => {
                 if (err) throw err;
-              });
+            }))
+
         })
 
 let teamHTML = render(teamMembers)
-    
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
